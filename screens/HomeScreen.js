dfc25cf10c51c49f9194a3766b9393f9907950ca
DebjeetDam <debjeet.dam95@gmail.com>
2024-06-26 22:05:35 -0500

    Categories

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,6 +5,7 @@ import {
   Image,
   ViewComponent,
   TextInput,
+  ScrollView,
 } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
@@ -14,6 +15,7 @@ import {
   AdjustmentsVerticalIcon,
   MagnifyingGlassIcon,
 } from "react-native-heroicons/outline";
+import Categories from "./components/Categories";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -27,42 +29,47 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView className="bg-white pt-5">
-      <Text className="text-red-500">
-        {/* Header */}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4 w-full">
-          <Image
-            source={{
-              uri: "https://links.papareact.com/wru",
-            }}
-            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+      {/* Header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+          <MagnifyingGlassIcon color="#00CCBB" />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
           />
-
-          <View className="flex-1">
-            <Text className="font-bold text-gray-400 text-xs">
-              Deliver Now!
-            </Text>
-            <Text className="font-bold text-xl">
-              Current Location
-              <ChevronDownIcon size={20} color="#00CCBB" />
-            </Text>
-          </View>
-
-          <UserIcon size={35} color="#00CCBB" />
         </View>
+        <AdjustmentsVerticalIcon color="#00CCBB" />
+      </View>
 
-        {/* Search */}
-        <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4 w-full">
-          <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
-            <MagnifyingGlassIcon color="#00CCBB" />
-            <TextInput
-              placeholder="Restaurants and cuisines"
-              keyboardType="default"
-            />
-          </View>
-
-          <AdjustmentsVerticalIcon color="#00CCBB" />
-        </View>
-      </Text>
+      {/* Body */}
+      <ScrollView
+        className="bg-gray-100"
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        {/* Categories */}
+        <Categories />
+        {/* Featured Rows */}
+      </ScrollView>
     </SafeAreaView>
   );
 };
