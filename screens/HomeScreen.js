1a1aeac0773f9ab2162e1ed65dd9df486e03ea69
DebjeetDam <debjeet.dam95@gmail.com>
2024-06-26 21:12:58 -0500

    HomeScreen headers

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,24 @@
-import { View, Text, SafeAreaView, Image, ViewComponent } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  ViewComponent,
+  TextInput,
+} from "react-native";
 import React, { useLayoutEffect } from "react";
-
 import { useNavigation } from "@react-navigation/native";
+import {
+  ChevronDownIcon,
+  UserIcon,
+  AdjustmentsVerticalIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
 
+  // Modify Header Settings
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
@@ -13,20 +26,43 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView>
-      {/* Header*/}
-      <View>
-        <Image
-          source={{
-            uri: "https://links.papareact.com/wru",
-          }}
-          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-        />
+    <SafeAreaView className="bg-white pt-5">
+      <Text className="text-red-500">
+        {/* Header */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4 w-full">
+          <Image
+            source={{
+              uri: "https://links.papareact.com/wru",
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
 
-        <View>
-          <Text>Deliver Now!</Text>
+          <View className="flex-1">
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver Now!
+            </Text>
+            <Text className="font-bold text-xl">
+              Current Location
+              <ChevronDownIcon size={20} color="#00CCBB" />
+            </Text>
+          </View>
+
+          <UserIcon size={35} color="#00CCBB" />
         </View>
-      </View>
+
+        {/* Search */}
+        <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4 w-full">
+          <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+            <MagnifyingGlassIcon color="#00CCBB" />
+            <TextInput
+              placeholder="Restaurants and cuisines"
+              keyboardType="default"
+            />
+          </View>
+
+          <AdjustmentsVerticalIcon color="#00CCBB" />
+        </View>
+      </Text>
     </SafeAreaView>
   );
 };
