bcbef2adfbd4121e89ec59a0d65a88a725ea1aae
DebjeetDam <debjeet.dam95@gmail.com>
2024-06-27 15:46:58 -0500

    sanity client config

---
+++ sanity.js
@@ -0,0 +1,20 @@
+// sanity.js
+import { createClient } from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+// Import using ESM URL imports in environments that supports it:
+// import {createClient} from 'https://esm.sh/@sanity/client'
+
+export const client = createClient({
+  projectId: "5njtpkk4",
+  dataset: "production",
+  useCdn: true, // set to `false` to bypass the edge cache
+  apiVersion: "2024-06-27", // use current date (YYYY-MM-DD) to target the latest API version
+  // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
+});
+
+const builder = imageUrlBuilder(client);
+export const urlFor = (source) => builder.image(source);
+
+// http://localhost:8081/
+
+export default client;
