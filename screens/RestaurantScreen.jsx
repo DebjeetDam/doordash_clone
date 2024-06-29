acb87539a6938ecded7823d2994977220488d17a
DebjeetDam <debjeet.dam95@gmail.com>
2024-06-28 19:35:44 -0500

    Restaurant Screen final

--- screens/RestaurantScreen.jsx
+++ screens/RestaurantScreen.jsx
@@ -10,7 +10,15 @@ import React, { useLayoutEffect, useState, useEffect } from "react";
 import { useRoute } from "@react-navigation/native";
 import { useNavigation } from "@react-navigation/native";
 import { urlFor } from "../sanity";
-import { ArrowLeftIcon } from "react-native-heroicons/solid";
+import {
+  ArrowLeftIcon,
+  MapIcon,
+  MapPinIcon,
+  QuestionMarkCircleIcon,
+  StarIcon,
+} from "react-native-heroicons/solid";
+import { ChevronRightIcon } from "react-native-heroicons/outline";
+import DishRow from "./components/DishRow";
 
 const RestaurantScreen = () => {
   const {
@@ -49,6 +57,48 @@ const RestaurantScreen = () => {
         >
           <ArrowLeftIcon size={20} color={"#00CCBB"} />
         </Pressable>
+      </View>
+
+      <View className="bg-white">
+        <View className="px-4 pt-4">
+          <Text className="text-3xl font-bold">{name}</Text>
+
+          <View className="flex-row space-x-2 my-1">
+            <View className="flex-row items-center space-x-1">
+              <StarIcon color="green" opacity={0.5} size={22} />
+              <Text className="text-xs text-gray-500">
+                <Text className="text-green-500">{rating}</Text> . {genre}
+              </Text>
+            </View>
+
+            <View className="flex-row items-center space-x-1">
+              <MapPinIcon color="gray" opacity={0.4} size={22} />
+              <Text className="text-xs text-gray-500">
+                <Text className="text-green-500">
+                  Nearby . {address.split(",")[0]}
+                </Text>
+              </Text>
+            </View>
+          </View>
+          <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
+        </View>
+        {/* Allergy Section */}
+        <Pressable className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+          <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
+          <Text className="pl-2 flex-1 text-md font-bold">
+            Have a food allergy?
+          </Text>
+          <ChevronRightIcon color="#00CCBB" />
+        </Pressable>
+      </View>
+
+      <View>
+        <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+
+        {/* Dishrow */}
+        {dishes.map((dish) => (
+          <DishRow />
+        ))}
       </View>
     </ScrollView>
   );
