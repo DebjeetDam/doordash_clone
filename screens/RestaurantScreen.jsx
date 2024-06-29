defb3d9d448769f2347fd9bcf4dfb385b80c5f31
DebjeetDam <debjeet.dam95@gmail.com>
2024-06-28 20:13:25 -0500

    Restaurant Screen updates

--- screens/RestaurantScreen.jsx
+++ screens/RestaurantScreen.jsx
@@ -1,11 +1,4 @@
-import {
-  View,
-  Text,
-  Image,
-  ScrollView,
-  TouchableOpacity,
-  Pressable,
-} from "react-native";
+import { View, Text, Image, ScrollView, TouchableOpacity } from "react-native";
 import React, { useLayoutEffect, useState, useEffect } from "react";
 import { useRoute } from "@react-navigation/native";
 import { useNavigation } from "@react-navigation/native";
@@ -51,12 +44,12 @@ const RestaurantScreen = () => {
           source={{ uri: urlFor(imgUrl).url() }}
           className="w-full h-56 bg-gray-300 p-4"
         />
-        <Pressable
+        <TouchableOpacity
           onPress={navigation.goBack}
           className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full"
         >
           <ArrowLeftIcon size={20} color={"#00CCBB"} />
-        </Pressable>
+        </TouchableOpacity>
       </View>
 
       <View className="bg-white">
@@ -83,13 +76,13 @@ const RestaurantScreen = () => {
           <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
         </View>
         {/* Allergy Section */}
-        <Pressable className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+        <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
           <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
           <Text className="pl-2 flex-1 text-md font-bold">
             Have a food allergy?
           </Text>
           <ChevronRightIcon color="#00CCBB" />
-        </Pressable>
+        </TouchableOpacity>
       </View>
 
       <View>
@@ -97,7 +90,14 @@ const RestaurantScreen = () => {
 
         {/* Dishrow */}
         {dishes.map((dish) => (
-          <DishRow />
+          <DishRow
+            key={dish._id}
+            id={dish._id}
+            name={dish.name}
+            description={dish.short_description}
+            price={dish.price}
+            image={dish.image}
+          />
         ))}
       </View>
     </ScrollView>
