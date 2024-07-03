38acc9a2a8ab29463d105ebd3a4f3722a64c295d
DebjeetDam <debjeet.dam95@gmail.com>
2024-07-02 20:45:57 -0500

    basketIcon change implemented

--- features/basketSlice.js
+++ features/basketSlice.js
@@ -31,7 +31,12 @@ export const basketSlice = createSlice({
 // Action creators are generated for each case reducer function
 export const { addToBasket, removeFromBasket } = basketSlice.actions;
 
+export const selectBasketItems = (state) => state.basket.items;
+
 export const selectBasketItemsWithId = (state, id) =>
   state.basket.items.filter((item) => item.id === id);
+
+export const selectBasketTotal = (state) =>
+  state.basket.items.reduce((total, item) => (total += item.price), 0);
 
 export default basketSlice.reducer;
